faa515fb605606be824f008b999cbe829d60aa51
Ambashadur <63015238+Ambashadur@users.noreply.github.com>
2025-02-04 15:56:39 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Breakout game recreation using Monogame
---
+++ src/Mono.Breakout/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Mono.Breakout;
+
+internal class Program
+{
+    private static void Main(string[] args) {
+        using var game = new GameExec();
+        game.Run();
+    }
+}

906442b22dc2a696b2ebd8305834179a7a27008f
Ambashadur <63015238+Ambashadur@users.noreply.github.com>
2025-02-04 17:45:47 +0300

    Add player bar movement

--- src/Mono.Breakout/GameExec.cs
+++ src/Mono.Breakout/GameExec.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,6 +7,10 @@ namespace Mono.Breakout;
 
 public class GameExec : Game
 {
+    private const float _barSpeed = 250;
+
+    private Rectangle _bar;
+    private Texture2D _barTexture;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -24,14 +29,23 @@ public class GameExec : Game
     protected override void LoadContent() {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        _barTexture = new Texture2D(GraphicsDevice, 1, 1);
+        _barTexture.SetData([Color.Chocolate]);
+        _bar = new Rectangle(100, 300, 100, 20);
     }
 
     protected override void Update(GameTime gameTime) {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        if (IsPressed(PlayerIndex.One, Buttons.Back) || IsPressed(Keys.Escape)) Exit();
 
-        // TODO: Add your update logic here
+        var state = Keyboard.GetState();
+        if (state.IsKeyDown(Keys.Right)) {
+            _bar.X += (int)(_barSpeed * gameTime.ElapsedGameTime.TotalSeconds);
+        } else if (state.IsKeyDown(Keys.Left)) {
+            _bar.X -= (int)(_barSpeed * gameTime.ElapsedGameTime.TotalSeconds);
+        }
+
+        if (_bar.X < 0) _bar.X = 0;
+        else if (_bar.Right >= _graphics.PreferredBackBufferWidth) _bar.X = _graphics.PreferredBackBufferWidth - _bar.Width;
 
         base.Update(gameTime);
     }
@@ -39,8 +53,19 @@ public class GameExec : Game
     protected override void Draw(GameTime gameTime) {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_barTexture, _bar, Color.White);
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool IsPressed(Keys key) => Keyboard.GetState().IsKeyDown(key);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool IsPressed(PlayerIndex player, Buttons button) =>
+        GamePad.GetState(player).IsButtonDown(button);
 }
